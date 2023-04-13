2142372f25b467042a4370125e7ebbe6d9012c89
sedighi-unfallpaten <117832385+sedighi-unfallpaten@users.noreply.github.com>
2023-04-14 00:09:19 +0200

    new_version

--- assets/js/newsletter/post-newsletter.js
+++ assets/js/newsletter/post-newsletter.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $("#newsletter").submit(function (event) {
+    $("#newsletter").submit(function(event) {
         event.preventDefault();
     });
 
-    $("#submit-newsletter").click(function () {
+    $("#submit-newsletter").click(function() {
         if ($('#email').val().length === 0) {
             return;
         }
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
 function postNewsletterAjaxRequest() {
 
-    $(document).ajaxStart(function () {
+    $(document).ajaxStart(function() {
         Pace.restart();
     });
     var overlay = $("#overlay");
@@ -23,13 +23,13 @@ function postNewsletterAjaxRequest() {
     overlay.show();
 
     setTimeout(
-        function () {
+        function() {
             $.ajax({
                 type: "PUT",
                 contentType: 'application/x-www-form-urlencoded',
                 url: "https://c1mntyc8ac.execute-api.eu-central-1.amazonaws.com/unfallpaten-service/api/newsletters",
                 data: $('form[name=newsletter]').serialize(),
-                complete: function (xhr, status) {
+                complete: function(xhr, status) {
                     if (xhr.readyState === 4 && xhr.status === 200) {
                         location.replace("https://www.unfallpaten.de/erfolg-eingang.html");
                     } else {
@@ -40,3 +40,39 @@ function postNewsletterAjaxRequest() {
 
         }, 3000);
 }
+
+// $(document).ready(function() {
+
+//     $("#newsletter").submit(function(event) {
+//         event.preventDefault();
+//         postNewsletterAjaxRequest();
+//     });
+
+// });
+
+// function postNewsletterAjaxRequest() {
+
+//     $(document).ajaxStart(function() {
+//         Pace.restart();
+//     });
+
+//     var overlay = $("#overlay");
+//     overlay.height($(document).height());
+//     overlay.show();
+
+//     setTimeout(function() {
+//         $.ajax({
+//             type: "PUT",
+//             contentType: 'application/x-www-form-urlencoded',
+//             url: "https://c1mntyc8ac.execute-api.eu-central-1.amazonaws.com/unfallpaten-service/api/newsletters",
+//             data: $('form[name=newsletter]').serialize(),
+//             complete: function(xhr, status) {
+//                 if (xhr.readyState === 4 && xhr.status === 200) {
+//                     location.replace("https://www.unfallpaten.de/erfolg-eingang.html");
+//                 } else {
+//                     location.replace("https://www.unfallpaten.de/fehler.html");
+//                 }
+//             },
+//         });
+//     }, 3000);
+// }
